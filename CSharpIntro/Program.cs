87866f63a2d060e0ac7084e759f320d547b5dc26
Emre <emre_5294@hotmail.com>
2021-01-24 11:31:21 +0300

    Product List Using For,Foreach and While Loops

--- CSharpIntro/Program.cs
+++ CSharpIntro/Program.cs
@@ -6,12 +6,47 @@ namespace CSharpIntro
     {
         static void Main(string[] args)
         {
-            MyList<string> name = new MyList<string>();
-            name.AddList("Emre");
-            name.AddList("Celik");
-            name.AddList("27");
-            name.AddList("EEM");
-           
+            Product product1 = new Product() { Id = 265, Name = "Pen", Unit = 50 };
+            Product product2 = new Product() { Id = 265, Name = "Eraser", Unit = 21 };
+            Product product3 = new Product() { Id = 785, Name = "Notebook", Unit = 12 };
+            Product[] products = new Product[3] { product1, product2, product3 };
+            //Using for loop display items
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.Write(products[i].Id+" ");
+                
+                Console.Write(products[i].Name+" ");
+               
+                Console.Write(products[i].Unit+" ");
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("                   ");
+            Console.WriteLine("******************");
+            //Using foreach loop display items
+            foreach (var product in products)
+            {
+                Console.Write(product.Id+" ");
+                Console.Write(product.Name + " ");
+
+                Console.Write(product.Unit + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("********");
+            //using while loop display items
+            int length = 0;
+            while (length<products.Length)
+            {
+                Console.Write(products[length].Id + " ");
+
+                Console.Write(products[length].Name + " ");
+
+                Console.Write(products[length].Unit + " ");
+                Console.WriteLine();
+                length++;
+
+            }
         }
     }
 }
